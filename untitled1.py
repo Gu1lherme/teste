9bdd750a67f20f6acc00547119f43c95fe9bcf89
aluno <guilhermemarinhocosta1701@gmail.com>
2017-09-26 21:19:07 -0400

    starting the project

--- untitled1.py
+++ untitled1.py
@@ -1,12 +1,12 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def hello_world():
-    return 'Hello World!'
+    return render_template('index.html')
 
 
-if __name__ == '__main__':0
+if __name__ == '__main__':
     app.run()
